e8d87fa124406382a96eeabf2a215e9dafac8792
travelite <rendrabootcamp@gmail.com>
2022-12-05 18:04:27 +0700

    pages mentee log done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const Navbar = () => {
+const Navbar = ({ namePages }) => {
   return (
-    <div className="w-full">
+    <div className="w-full overflow-hidden ">
       <div className="flex flex-col p-5">
         <p className="text-3xl font-bold text-dark-alta ">
           Immersive Dashboard
         </p>
         <div className="flex justify-between">
-          <p className="text-xl font-bold text-orange-alta">Dashboard</p>
+          <p className="text-xl font-bold text-orange-alta">{namePages}</p>
           <p className="text-xl text-dark-alta/50 font-light">
             Hello, <span className="text-dark-alta">John doe</span>
           </p>
