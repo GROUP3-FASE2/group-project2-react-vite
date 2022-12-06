b97ddf975b2bc7b9486dcd2ac6f2b0285dc590d2
travelite <rendrabootcamp@gmail.com>
2022-12-06 23:53:35 +0700

    fixed blankPage & implement SweetAlert

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,11 +7,12 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { updateUser } from "../store/features/usersSlice";
 import { useCookies } from "react-cookie";
+import Swal from "sweetalert2";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [msg, setMsg] = useState("");
+  const [msg] = useState("");
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [cookies, setCookie] = useCookies(["userToken"]);
@@ -19,13 +20,20 @@ const Login = () => {
   const authLogin = async (e) => {
     e.preventDefault();
     await axios
-      .post(`http://35.194.55.171:8000/auth`, {
+      .post(`http://34.136.159.229:8000/auth`, {
         email: email,
         password: password,
       })
       .then((response) => {
         const { data } = response.data;
         if (data) {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            text: "Signed successfully",
+            showConfirmButton: false,
+            timer: 1500,
+          });
           setCookie("userToken", data.token, { path: "/" });
           dispatch(updateUser(data));
           navigate("/dashboard");
@@ -33,8 +41,14 @@ const Login = () => {
       })
 
       .catch((error) => {
+        Swal.fire({
+          position: "center",
+          icon: "error",
+          title: "Email or Password incorrect",
+          showConfirmButton: true,
+        });
         console.log("err msg :", error);
-        setMsg(error);
+        // setMsg(error);
       });
   };
 
@@ -47,11 +61,11 @@ const Login = () => {
 
   return (
     <Container>
-      <div className="flex flex-col w-full h-screen bg-dark-alta">
+      <div className="relative flex flex-col w-full h-screen bg-dark-alta">
         <div className="m-5 w-full">
           <img src={logo} alt="" className="w-32 mx-5" />
         </div>
-        <div className="w-3/4 mx-10">
+        <div className="w-3/4 mx-10 z-10">
           <h1 className="mt-5 text-6xl font-bold text-white">Welcome...</h1>
           <p className="my-5 text-white">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
@@ -59,19 +73,11 @@ const Login = () => {
             maxime iste dicta?
           </p>
         </div>
-        <div className="w-full h-full">
+        <div className="w-full h-full absolute pt-44 ml-24">
           <img src={login} alt="" className="block m-auto" />
         </div>
       </div>
       <div className="flex flex-col w-full h-screen bg-dark-alta">
-        {/* <div>
-                    <form>
-                        <div>
-                            <label>Email</label>
-                            <input />
-                        </div>
-                    </form>
-                </div> */}
         <div className="flex h-screen justify-center items-center">
           <div className="flex flex-col justify-center w-[400px]">
             <form
@@ -115,7 +121,7 @@ const Login = () => {
               </div>
               <button
                 type="submit"
-                className="w-full mt-10 py-2 bg-orange-alta shadow-lg text-white font-semibold rounded-lg"
+                className="w-[90%] flex m-auto mt-10 py-2 bg-orange-alta shadow-lg text-white font-semibold rounded-lg btn hover:bg-orange-600"
               >
                 LOGIN
               </button>
